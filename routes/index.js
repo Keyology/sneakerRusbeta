51807509dab1d9d655d2bdd35df70a74835e3543
Keoni Murray <keonimurray45@gmail.com>
2018-12-22 19:22:42 -0500

    added node

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+module.exports = (app) => {
+
+    app.get('/', (req, res) => {
+        // get and render html to client
+        res.render('index')
+
+    });
+
+    app.get('/signup', (req, res) => {
+        // get and render signup page
+        res.render('signup')
+    });
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 5000;
+const mainroutes = require('./routes/index.js');
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+
+mainroutes(app);
+
+app.listen(port, () => console.log(`listening on port: ${port}`))
+
+module.exports = app;
